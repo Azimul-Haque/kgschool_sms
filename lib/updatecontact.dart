94f670bf3d8eb96fbb77f0dab66f71e0e330965a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 22:08:06 +0600

    Commit Mon 07/25/2022 22:08:06.19 24038

--- lib/updatecontact.dart
+++ lib/updatecontact.dart
@@ -7,10 +7,7 @@ import 'package:kgschool_sms/models/contact_model.dart';
 
 class UpdateContact extends StatefulWidget {
   final List<ContactModel> contact;
-  const UpdateContact(
-    ContactModel contact, {
-    Key? key,
-  }) : super(key: key);
+  const UpdateContact(this.contact);
   // const UpdateContact({Key? key}) : super(key: key);
 
   @override
