c949cc66c9b63b3a4e68da78a5f92905299b9607
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-26 02:17:56 +0600

    Commit Tue 07/26/2022  2:17:56.15 24956

--- lib/home.dart
+++ lib/home.dart
@@ -83,7 +83,7 @@ class _HomePageState extends State<HomePage> {
               ExcelToJson().convert().then((onValue) {
                 // print(jsonEncode(onValue).length);
                 setState(() {
-                  parseddata = onValue;
+                  parseddata = onValue!;
                   // parseddata = onValue.split(",");
                   print(parseddata[0]);
                 });
