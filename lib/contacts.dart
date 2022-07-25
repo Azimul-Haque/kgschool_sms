3f8c6536bf43cb607429b760444da2a22725a798
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-26 02:59:32 +0600

    Commit Tue 07/26/2022  2:59:32.19 31250

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:kgschool_sms/globals.dart';
 // import 'package:flutter_sms/flutter_sms.dart';
 import 'package:kgschool_sms/models/contact_model.dart';
 import 'package:kgschool_sms/updatecontact.dart';
