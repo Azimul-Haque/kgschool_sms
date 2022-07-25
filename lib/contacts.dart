197f88d30f0b48abc940550697af9a47655524f3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-26 00:56:12 +0600

    Commit Tue 07/26/2022  0:56:12.20 23236

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -188,4 +188,32 @@ class _ContactsListState extends State<ContactsList> {
     });
     print(adhoccontacts.length);
   }
+
+  Future<void> show(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('This is a demo alert dialog.'),
+                Text('Would you like to approve of this message?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
