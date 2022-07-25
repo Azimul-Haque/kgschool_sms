3d668a8ba468fc6c890bfa37f9f304206e565f56
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 17:49:12 +0600

    Commit Mon 07/25/2022 17:49:12.17 22943

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:kgschool_sms/home.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  final database = openDatabase(
+    // Set the path to the database. Note: Using the `join` function from the
+    // `path` package is best practice to ensure the path is correctly
+    // constructed for each platform.
+    join(await getDatabasesPath(), 'doggie_database.db'),
+  );
   runApp(const MyApp());
 }
 
