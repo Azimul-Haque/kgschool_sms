d0554132217f5725cbe0c9758a6191fbfac40c9e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 17:35:44 +0600

    Commit Mon 07/25/2022 17:35:44.14 6312

--- lib/models/contact_model.dart
+++ lib/models/contact_model.dart
@@ -8,14 +8,11 @@ const String columnName = "name";
 const String columnContactNumber = "contactnumber";
 
 class ContactModel {
-  int id;
+  final int id;
   final String name;
   final String contactnumber;
 
-  ContactModel(
-    this.name,
-    this.contactnumber,
-  );
+  ContactModel(this.name, this.contactnumber, {id, name, contactnumber});
 
   Map<String, dynamic> toMap() {
     return {
