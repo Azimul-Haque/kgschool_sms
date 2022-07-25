8a38fa6a84e8ede15d4872b4df768c396fb81c71
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 19:30:15 +0600

    Commit Mon 07/25/2022 19:30:15.19 30366

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -42,7 +42,7 @@ class _ContactsListState extends State<ContactsList> {
             children: [
               const Card(
                 child: ListTile(
-                  title: Text('মোট নাম্বারঃ ' + 454),
+                  title: Text(contacts.length.toString()),
                 ),
                 margin: EdgeInsets.only(top: 10, left: 10, right: 10),
               ),
