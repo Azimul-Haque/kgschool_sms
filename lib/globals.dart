98e74f8247eafba4bc5e5aa9077f3b384f1eaa44
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 19:14:33 +0600

    Commit Mon 07/25/2022 19:14:33.19 19146

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,9 +3,11 @@ import 'package:sqflite/sqlite_api.dart';
 
 late Database? db;
 
-void createSnackBar(String message) {
-  final snackBar = new SnackBar(content: new Text(message));
-
-  // Find the Scaffold in the Widget tree and use it to show a SnackBar!
-  Scaffold.of(scaffoldContext).showSnackBar(snackBar);
+showSimpleSnackBar(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      behavior: SnackBarBehavior.floating,
+      content: Text(message),
+    ),
+  );
 }
