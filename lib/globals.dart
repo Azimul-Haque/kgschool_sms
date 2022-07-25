77bda6144943e8d70c707ec965444d0c7fc4a73e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 19:13:51 +0600

    Commit Mon 07/25/2022 19:13:51.22 28905

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,13 +1,10 @@
-import 'dart:html';
-
 import 'package:sqflite/sqlite_api.dart';
 
 late Database? db;
 
-void createSnackBar(String message) {                                                                               
-  final snackBar = new SnackBar(content: new Text(message));                                                                                      
+void createSnackBar(String message) {
+  final snackBar = new SnackBar(content: new Text(message));
 
-  // Find the Scaffold in the Widget tree and use it to show a SnackBar!                                            
-  Scaffold.of(scaffoldContext).showSnackBar(snackBar);                                                              
-  }                                                                                                                   
-} 
+  // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+  Scaffold.of(scaffoldContext).showSnackBar(snackBar);
+}
