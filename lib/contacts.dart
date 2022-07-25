929a1a666a1b75d21b09435fb9c85f621083b12a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 23:13:09 +0600

    Commit Mon 07/25/2022 23:13:09.25 14465

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -17,7 +17,7 @@ class _ContactsListState extends State<ContactsList> {
   final ContactHelper _contactHelper = ContactHelper();
   List<ContactModel> contacts = [];
   late bool isLoading;
-  bool refresh = await Navigator.push(context, MaterialPageRoute(builder: (_) => UpdateContact("Foo")));;
+  bool refresh = await Navigator.push(context, MaterialPageRoute(builder: (_) => const UpdateContact(1, '1', '1')));
 
   @override
   void initState() {
