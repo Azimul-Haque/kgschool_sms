44377f91f4eed4bb13b291758337b7223308f43f
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 14:19:04 +0600

    2nd Commit
    Signed-off-by: Azimul-Haque <orbachinujbuk@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'KG School SMS 1.0',
+      title: 'KG School SMS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
