0204c4c901f7df8c705f60bc3fc661364cfff792
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 23:02:02 +0600

    Commit Mon 07/25/2022 23:02:02.20 20837

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -71,7 +71,7 @@ class _ContactsListState extends State<ContactsList> {
                                           contacts[index].id,
                                           contacts[index].name,
                                           contacts[index].contactnumber)),
-                                ).then((value) { setState(() {};
+                                ).then((value) { setState(() {})
                               },
                               icon: const Icon(Icons.edit),
                             ),
