686f15addc8c2b0b1077088d5ba7266b4d755b27
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 16:23:01 +0600

    Commit Mon 07/25/2022 16:23:01.20 13692

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -42,48 +42,6 @@ class _ContactsListState extends State<ContactsList> {
     }
   }
 
-  Future<bool> _canSendSMS() async {
-    bool _result = await canSendSMS();
-    setState(() => _canSendSMSMessage =
-        _result ? 'This unit can send SMS' : 'This unit cannot send SMS');
-    return _result;
-  }
-
-  Widget _phoneTile(String name) {
-    return Padding(
-      padding: const EdgeInsets.all(3),
-      child: Container(
-          decoration: BoxDecoration(
-              border: Border(
-            bottom: BorderSide(color: Colors.grey.shade300),
-            top: BorderSide(color: Colors.grey.shade300),
-            left: BorderSide(color: Colors.grey.shade300),
-            right: BorderSide(color: Colors.grey.shade300),
-          )),
-          child: Padding(
-            padding: const EdgeInsets.all(4),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () => setState(() => people.remove(name)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(0),
-                  child: Text(
-                    name,
-                    textScaleFactor: 1,
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                )
-              ],
-            ),
-          )),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -98,21 +56,6 @@ class _ContactsListState extends State<ContactsList> {
         ),
         body: ListView(
           children: <Widget>[
-            if (people.isEmpty)
-              const SizedBox(height: 0)
-            else
-              SizedBox(
-                height: 90,
-                child: Padding(
-                  padding: const EdgeInsets.all(3),
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: List<Widget>.generate(people.length, (int index) {
-                      return _phoneTile(people[index]);
-                    }),
-                  ),
-                ),
-              ),
             ListTile(
               leading: const Icon(Icons.people),
               title: TextField(
@@ -142,17 +85,6 @@ class _ContactsListState extends State<ContactsList> {
               ),
             ),
             const Divider(),
-            ListTile(
-              title: const Text('Can send SMS'),
-              subtitle: Text(_canSendSMSMessage),
-              trailing: IconButton(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                icon: const Icon(Icons.check),
-                onPressed: () {
-                  _canSendSMS();
-                },
-              ),
-            ),
             SwitchListTile(
                 title: const Text('Send Direct'),
                 subtitle: const Text(
