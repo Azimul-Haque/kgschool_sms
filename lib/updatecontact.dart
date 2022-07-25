080ee6b402c210cf856c08f057455072e291857e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 22:00:34 +0600

    Commit Mon 07/25/2022 22:00:34.16 9377

--- lib/updatecontact.dart
+++ lib/updatecontact.dart
@@ -6,6 +6,8 @@ import 'package:kgschool_sms/globals.dart';
 import 'package:kgschool_sms/models/contact_model.dart';
 
 class UpdateContact extends StatefulWidget {
+  final List contact;
+  OTPScreen(this.phone);
   const UpdateContact({Key? key}) : super(key: key);
 
   @override
