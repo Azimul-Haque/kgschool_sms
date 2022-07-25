23a5118750309d48e1ea404175520385574ce7cf
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 21:56:38 +0600

    Commit Mon 07/25/2022 21:56:38.24 11252

--- lib/models/contact_model.dart
+++ lib/models/contact_model.dart
@@ -73,7 +73,7 @@ class ContactHelper {
     final db = await database;
 
     await db!.update(
-      'dogs',
+      'contacts',
       contact.toMap(),
       where: 'id = ?',
       whereArgs: [contact.id],
