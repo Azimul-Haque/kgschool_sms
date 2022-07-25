c340cc25588e272a3593057180f3a333d4ad708e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-26 03:27:32 +0600

    Commit Tue 07/26/2022  3:27:32.27 22037

--- lib/home.dart
+++ lib/home.dart
@@ -171,7 +171,7 @@ class _HomePageState extends State<HomePage> {
                       toolbarOptions:
                           const ToolbarOptions(copy: true, paste: true),
                       minLines: 3,
-                      maxLines: 10,
+                      maxLines: 5,
                       autovalidateMode: AutovalidateMode.always,
                       decoration: const InputDecoration(
                         icon: Icon(Icons.sms),
