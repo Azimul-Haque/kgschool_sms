d72e9edaabda897b326346d28365c769b5d420ec
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 21:58:14 +0600

    Commit Mon 07/25/2022 21:58:14.15 4356

---
+++ lib/updatecontact.dart
@@ -0,0 +1,116 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
+import 'package:kgschool_sms/globals.dart';
+import 'package:kgschool_sms/models/contact_model.dart';
+
+class AddContact extends StatefulWidget {
+  const AddContact({Key? key}) : super(key: key);
+
+  @override
+  _AddContactState createState() => _AddContactState();
+}
+
+class _AddContactState extends State<AddContact> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerContactNumber =
+      TextEditingController();
+  String? name, contactnumber;
+  late ContactModel newContact;
+  final ContactHelper _contactHelper = ContactHelper();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text('Add Contact'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: TextFormField(
+                  controller: _controllerName,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                  keyboardType: TextInputType.text,
+                  onChanged: (String value) => setState(() {
+                    name = value;
+                  }),
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Name is empty';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.phone),
+                title: TextFormField(
+                  decoration: const InputDecoration(labelText: 'Mobile Number'),
+                  controller: _controllerContactNumber,
+                  keyboardType: TextInputType.number,
+                  maxLength: 11,
+                  onChanged: (String value) => setState(() {
+                    contactnumber = value;
+                  }),
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Mobile Number is empty';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const Divider(),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith(
+                        (states) => Theme.of(context).colorScheme.secondary),
+                    padding: MaterialStateProperty.resolveWith(
+                        (states) => const EdgeInsets.symmetric(vertical: 16)),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _addContact();
+                    }
+                  },
+                  child: const Text('Add Contact'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _addContact() async {
+    newContact = ContactModel(
+      id: 1,
+      name: name.toString(),
+      contactnumber: contactnumber.toString(),
+    );
+    await _contactHelper.insertContact(newContact);
+    showSimpleSnackBar(context, 'Added successfully!');
+    Navigator.pop(context);
+  }
+}
