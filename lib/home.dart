780a3c73ac8198a7fcfae32f49c00a1ee72cc15d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-26 02:33:41 +0600

    Commit Tue 07/26/2022  2:33:41.25 559

--- lib/home.dart
+++ lib/home.dart
@@ -87,7 +87,7 @@ class _HomePageState extends State<HomePage> {
                   // parseddata = onValue.split(",");
                   List decodeddata = jsonDecode(onValue!);
                   // print('${decodeddata.runtimeType} : $decodeddata');
-                  print(decodeddata.toString());
+                  print(decodeddata.length);
                 });
               });
             },
