d3231d441913003a107d01a6ead2a98beade3f9a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-26 23:49:18 +0600

    Commit Tue 07/26/2022 23:49:18.22 15444

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:sqflite/sqflite.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await db!.execute("DROP TABLE IF EXISTS tableName");
   db = await openDatabase(join(await getDatabasesPath(), "contacts.db"),
       onCreate: (db, version) {
     return db.execute(
