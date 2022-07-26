e713a93ddeda9ea90c3a63637666f2230b5117fd
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 00:46:54 +0600

    Commit Wed 07/27/2022  0:46:54.21 9503

--- lib/home.dart
+++ lib/home.dart
@@ -286,7 +286,7 @@ class _HomePageState extends State<HomePage> {
                       autovalidateMode: AutovalidateMode.onUserInteraction,
                       decoration: const InputDecoration(
                         icon: Icon(Icons.calculate),
-                        hintText: ' +টেক্সট মেসেজ লিখুন',
+                        hintText: '$randomNumber1 টেক্সট মেসেজ লিখুন',
                         labelText: 'যোগফল',
                       ),
                       onChanged: (String value) {
