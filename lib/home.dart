2776626c5178fafbfda074f167f7cca80bdd98ba
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 01:01:44 +0600

    Commit Wed 07/27/2022  1:01:44.19 18566

--- lib/home.dart
+++ lib/home.dart
@@ -36,8 +36,8 @@ class _HomePageState extends State<HomePage> {
   int dropdownvalue = 0;
   final int randomNumber1 = Random().nextInt(10);
   final int randomNumber2 = Random().nextInt(10);
-  final int randomSum = randomNumber1 + randomNumber2;
-  static const String hint = 'যোগফল';
+  late int randomSum = randomNumber1 + randomNumber2;
+  static const String hint = randomNumber1.toString() + 'যোগফল';
 
   @override
   void initState() {
