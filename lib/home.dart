e07f058d6cb2ed9233d935387668e98f18eafb2f
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 00:37:14 +0600

    Commit Wed 07/27/2022  0:37:14.17 22112

--- lib/home.dart
+++ lib/home.dart
@@ -201,7 +201,7 @@ class _HomePageState extends State<HomePage> {
             children: <Widget>[
               Text(
                 'মোট নাম্বারঃ ' + recipients.length.toString(),
-                style: Theme.of(context).textTheme.headline1,
+                style: Theme.of(context).textTheme.bodyText1,
               ),
               const Divider(),
               Form(
